0f6495b152c8bf482e694f6cd8d99a08e893c9bf
lzegarra <luis_zegarra_rufino18@hotmail.com>
2023-12-09 10:59:37 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/ferreteria/web/FerreteriaWebApplication.java
@@ -0,0 +1,13 @@
+package com.ferreteria.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaWebApplication.class, args);
+	}
+
+}
